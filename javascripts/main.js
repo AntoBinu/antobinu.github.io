ee9cc28e31688bdea719f80b0cd76393bddf5776
Anto Binu Johnson <antobinu144@gmail.com>
2022-12-01 13:42:34 +0530

    adding contents and sldier

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+
+
+const portfolio =  {
+    init : function (){
+        console.log('Dont look here :)');
+        this.generate()
+    },
+    generate: function(){
+        $(".contents").text('null')
+        $(".next").on("click",function(){
+            portfolio.nextContent()
+        })
+        $(".prev").on("click",function(){
+            portfolio.prevContent()
+        })
+    },
+    nextContent:function(){
+        $(".contents").text("next")
+    },
+    prevContent:function(){
+       $(".contents").text("prev")
+    }
+
+
+
+}
+
+$(document).ready(
+    portfolio.init()
+)
